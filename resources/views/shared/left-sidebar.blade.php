9ad24680a5c0ca28d7157e0a327693600b667472
Nicolas Desjardins <nick.desjardins@live.com>
2023-12-08 17:50:26 -0500

    end of project dynamic local

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -16,6 +16,8 @@
         </ul>
     </div>
     <div class="card-footer text-center py-2">
-        <a class="btn btn-link btn-sm" href="{{ route('profile') }}">View Profile </a>
+        <a class="btn btn-link btn-sm" href="{{ route('lang', 'en') }}">en</a>
+        <a class="btn btn-link btn-sm" href="{{ route('lang', 'es') }}">es</a>
+        <a class="btn btn-link btn-sm" href="{{ route('lang', 'fr') }}">fr</a>
     </div>
 </div>
